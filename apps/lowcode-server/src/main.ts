c48aa915ea6cd197dab48bfd427a598331caccac
李云富 <liyunfu1998@qq.com>
2024-02-29 03:36:10 +0000

    feat: 统一返回格式

--- apps/lowcode-server/src/main.ts
+++ apps/lowcode-server/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 统一返回格式
+  app.useGlobalInterceptors(new TransformInterceptor())
+  
   // 接口版本化管理
   app.enableVersioning({
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
