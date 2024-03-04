405d5999a439e4f958e939c55b301dfe9cb8bbea
李云富 <liyunfu1998@qq.com>
2024-03-04 02:25:51 +0000

    feat: 新建user微服务且新建user下的user模块 使用 nest g resource user --project user

---
+++ apps/user/src/common/exceptions/base.exception.filter.ts
@@ -0,0 +1,20 @@
+// 处理统一异常
+
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus, ServiceUnavailableException } from "@nestjs/common";
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter{
+  catch(exception: Error, host:ArgumentsHost){
+    const ctx = host.switchToHttp()
+    const response =ctx.getResponse()
+    const request = ctx.getRequest()
+
+    // 非HTTP标准异常的处理
+    response.status(HttpStatus.SERVICE_UNAVAILABLE).json({
+      statusCode: HttpStatus.SERVICE_UNAVAILABLE,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: new ServiceUnavailableException().getResponse()
+    })
+  }
+}
---
+++ apps/user/src/common/exceptions/http.exception.filter.ts
@@ -0,0 +1,34 @@
+// HTTP标准请求异常
+
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { BusinessException } from "./business.exception";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter{
+  catch(exception: HttpException, host: ArgumentsHost){
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse()
+    const request = ctx.getRequest()
+    const status = exception.getStatus()
+
+    // 处理业务异常
+    if(exception instanceof BusinessException){
+      const error = exception.getResponse()
+      response.status(HttpStatus.OK).json({
+        data: null,
+        status: error['code'],
+        extra: {},
+        message: error['message'],
+        success: false
+      })
+      return 
+    }
+    
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.getResponse()
+    })
+  }
+}
